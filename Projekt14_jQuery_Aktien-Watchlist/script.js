259e451c1f76a99a31935a96d44cdf8382927620
pulle12 <psummerauer@tsn.at>
2026-03-04 14:51:08 +0100

    fix: clearing loadingText element when successful and fix the setting of the api return variables

--- Projekt14_jQuery_Aktien-Watchlist/script.js
+++ Projekt14_jQuery_Aktien-Watchlist/script.js
@@ -3,21 +3,23 @@ $(document).ready(function () {
 
     $('#addStockButton').click(function () {
         const stock = $('#stockInput').val();
-        const apiKey = 'APIKEY';
+        const apiKey = '';
         const apiUrl = `https://api.marketstack.com/v2/eod/latest?access_key=${apiKey}&symbols=${stock}`;
         $('#searchHistory').append(`<li>${stock}</li>`);
         $('#loadingText').html('<p>Lade Wetterdaten...</p>');
 
         $.get(apiUrl, function (data) {
-            const stockName = data.data.name;
-            const stockPrice = data.data.close;
+            const first = data.data[0];
+            const stockName = first.symbol;
+            const stockPrice = first.close;
             
             $('#stock-table-body').html(`
                 <tr>
                 <td>${stockName}</td>
                 <td>${stockPrice}</td>
-                <td><button id="">Entfernen</button></td>
+                <td><button class="remove-stock">Entfernen</button></td>
             `);
+            $('#loadingText').html();
         }).fail(function () {
             $('#loadingText').html('<p style="color: red;">Netzwerkfehler. Konnte die API nicht erreichen.</p>');
         }).always(function () {
